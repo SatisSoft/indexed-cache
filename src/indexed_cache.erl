03124a9f52c6e623fb52c344af31653d03aaae9a
dreyk <gunin@satissoft.ru>
2017-02-06 00:53:39 -0800

    cashe

--- src/indexed_cache.erl
+++ src/indexed_cache.erl
@@ -80,5 +80,4 @@ get(PoolId, Constrains, SortField, Order, Offset, Count, Aggregations) when is_l
 -spec update(PoolId :: atom(), GroupId :: binary(), Update::objects()) ->
     true.
 update(PoolId, GroupId, Update) ->
-    lager:info("Update ~p:~p",[GroupId,Update]),
     indexed_cache_request:update(PoolId, GroupId, Update).
